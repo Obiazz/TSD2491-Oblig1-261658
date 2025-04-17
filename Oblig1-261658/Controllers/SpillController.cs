83f021e3da6108f878b9a088a21019784bfa54da
Obiazz <azuolas.korsakas2@gmail.com>
2025-04-17 23:36:50 +0200

    Commit4a: la til spill i ny visning Spill2like

---
+++ Oblig1-261658/Controllers/SpillController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Oblig1_261658.Controllers
+{
+    public class SpillController : Controller
+    {
+        public IActionResult Spill2Like()
+        {
+            return View();
+        }
+    }
+}
